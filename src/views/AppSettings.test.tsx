e5c40d8122a54070bb6cf6ec36476bcf3e498060
Hunter Clarke <hunter@hunterclarke.me>
2022-11-12 12:29:09 -0700

    figures out better mocking for hooks

--- src/views/AppSettings.test.tsx
+++ src/views/AppSettings.test.tsx
@@ -1,27 +1,44 @@
 import { render, getMockContextProps } from '@stripe/ui-extension-sdk/testing'
-import { Box, TextField } from '@stripe/ui-extension-sdk/ui'
+import { Box, Spinner, TextField } from '@stripe/ui-extension-sdk/ui'
+import useToken from '../hooks/useToken'
 
 import AppSettings from './AppSettings'
 
 jest.mock('../hooks/useToken', () => ({
   __esmodule: true,
-  default: () => ({
-    data: {
-      token: 'abc123',
-    },
-  }),
+  default: jest.fn(),
 }))
 
 describe('AppSettings', () => {
-  it('renders SettingsView', async () => {
-    const { wrapper } = render(<AppSettings {...getMockContextProps()} />)
+  it('renders spinner when loading', () => {
+    ;(useToken as jest.Mock).mockImplementation(() => {
+      return {
+        isLoading: true,
+      }
+    })
 
+    const { wrapper } = render(<AppSettings {...getMockContextProps()} />)
     expect(wrapper.find(Box)).toContainText(
       'Use the access token below to request feature access flags via the Spackle API.',
     )
+    expect(wrapper.find(Spinner)).toBeTruthy()
+  })
 
+  it('renders token when loaded', async () => {
+    ;(useToken as jest.Mock).mockImplementation(() => {
+      return {
+        data: {
+          token: 'test-token',
+        },
+      }
+    })
+
+    const { wrapper } = render(<AppSettings {...getMockContextProps()} />)
+    expect(wrapper.find(Box)).toContainText(
+      'Use the access token below to request feature access flags via the Spackle API.',
+    )
     expect(wrapper.find(TextField)).toHaveProps({
-      value: 'abc123',
+      value: 'test-token',
     })
   })
 })
